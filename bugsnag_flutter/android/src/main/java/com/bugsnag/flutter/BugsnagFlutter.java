b12bea45e151925366401fd119a65f47aed99f90
Jason <jason.morris@bugsnag.com>
2022-03-02 12:12:04 +0000

    doc(android): explain the purpose and structure of the `BugsnagFlutter` class

--- bugsnag_flutter/android/src/main/java/com/bugsnag/flutter/BugsnagFlutter.java
+++ bugsnag_flutter/android/src/main/java/com/bugsnag/flutter/BugsnagFlutter.java
@@ -8,6 +8,15 @@ import com.bugsnag.android.Bugsnag;
 
 import org.json.JSONObject;
 
+/**
+ * This sits between the Flutter layer of function calls, and the native {@link Bugsnag} class.
+ * Each method in this class corresponds to a {@link BSGFunction} listed in
+ * {@link BugsnagFlutterPlugin}, and is responsible for unwrapping and wrapping the JSON arguments
+ * passed from and to the Flutter layer.
+ *
+ * In order to support the contract imposed by {@link BSGFunction} methods may need to return
+ * {@code Void} (the object) instead of {@code void} as would be typical.
+ */
 class BugsnagFlutter {
 
     Context context;
