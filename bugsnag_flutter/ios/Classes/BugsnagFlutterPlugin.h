eb11c9344a9830f70bb5d666481cc83baa383c33
Nick Dowell <nick@bugsnag.com>
2022-03-03 12:41:08 +0000

    Add BugsnagFlutterProtocol and use NSInvocation (#12)
    * Add BugsnagFlutterProtocol and use NSInvocation
    
    * Check numberOfArguments before invoking method

--- bugsnag_flutter/ios/Classes/BugsnagFlutterPlugin.h
+++ bugsnag_flutter/ios/Classes/BugsnagFlutterPlugin.h
@@ -1,13 +1,16 @@
 #import <Flutter/Flutter.h>
 
-@interface BugsnagFlutterPlugin : NSObject<FlutterPlugin>
-
-@property (nonatomic) NSSet<NSString *> *availableFunctions;
+/// Declares the methods exposed to Flutter
+@protocol BugsnagFlutterProtocol
 
 - (void)setUser:(NSDictionary *)json;
 - (NSDictionary *)getUser:(NSDictionary *)json;
 - (void)setContext:(NSDictionary *)json;
 - (NSString *)getContext:(NSDictionary *)json;
 - (void)attach:(NSDictionary *)json;
+
+@end
+
+@interface BugsnagFlutterPlugin : NSObject<FlutterPlugin, BugsnagFlutterProtocol>
 
 @end
