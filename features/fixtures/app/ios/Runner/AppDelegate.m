ca96e348399df3c589b33b43c99032b92e611629
Steve Kirkland-Walton <steve@bugsnag.com>
2022-03-07 22:51:06 +0000

    iOS side fetching the command

--- features/fixtures/app/ios/Runner/AppDelegate.m
+++ features/fixtures/app/ios/Runner/AppDelegate.m
@@ -16,7 +16,7 @@
     FlutterMethodChannel* nativeChannel = [FlutterMethodChannel methodChannelWithName:@"com.bugsnag.mazeRunner/platform"
               binaryMessenger:controller.engine.binaryMessenger
     ];
-    
+     
     [nativeChannel setMethodCallHandler:^(FlutterMethodCall *call, FlutterResult result) {
         [self onMethod :call :result];
     }];
@@ -27,7 +27,9 @@
 -(void)onMethod:(FlutterMethodCall*) call
                :(FlutterResult) result {
     
-    if([@"runScenario" isEqualToString:call.method]) {
+    if([@"getCommand" isEqualToString:call.method]) {
+        result([self getCommand]);
+    } else if([@"runScenario" isEqualToString:call.method]) {
         NSString *scenarioName = call.arguments[@"scenarioName"];
         NSString *extraConfig = call.arguments[@"extraConfig"];
         Scenario *targetScenario = [Scenario createScenarioNamed:scenarioName];
@@ -46,6 +48,26 @@
         [Bugsnag start];
         result(nil);
     }
+}
+
+
+-(NSString *)getCommand {
+//    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration ephemeralSessionConfiguration]];
+//    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://bs-local.com:9339/command"]];
+    NSURL *url = [NSURL URLWithString:@"http://bs-local.com:9339/command"];
+    NSData *data = [NSData dataWithContentsOfURL:url];
+    NSString *ret = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    
+//    [[session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+//        if (![response isKindOfClass:[NSHTTPURLResponse class]] || [(NSHTTPURLResponse *)response statusCode] != 200) {
+//            NSLog(@"%s request failed with %@", __PRETTY_FUNCTION__, response ?: error);
+//            return @"{}";
+//        }
+//        NSString *command = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+//        return command;
+//    }] resume];
+
+    return ret;
 }
 
 @end
