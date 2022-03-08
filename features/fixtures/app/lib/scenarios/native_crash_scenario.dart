dd429bff965e9e275e822b94022cb3dcbdc5227a
Steve Kirkland-Walton <steve@bugsnag.com>
2022-03-08 23:15:16 +0000

    Tidy-up

--- features/fixtures/app/lib/scenarios/native_crash_scenario.dart
+++ features/fixtures/app/lib/scenarios/native_crash_scenario.dart
@@ -5,6 +5,7 @@ import 'scenario.dart';
 class NativeCrashScenario extends Scenario {
   @override
   Future<void> run() async {
+    // TODO: I think I would factor startBugsnag() out of the scenarios and into main to avoid repetition
     await startBugsnag();
     await MazeRunnerChannels.runScenario("NativeCrashScenario");
   }
--- features/poc.feature
+++ features/poc.feature
@@ -1,6 +1,6 @@
 Feature: Reporting crash events
 
-  Scenario: Proof of concept
+  Scenario: A basic native crash
     When I run "NativeCrashScenario" and relaunch the crashed app
     And I configure Bugsnag for "NativeCrashScenario"
 #    And I wait to receive an error
