103796637e1ed0936f45afaf49e2333dd7a6b6fe
Steve Kirkland-Walton <steve@bugsnag.com>
2022-03-09 10:52:33 +0000

    Tidy-up

--- features/fixtures/app/ios/Runner/AppDelegate.m
+++ features/fixtures/app/ios/Runner/AppDelegate.m
@@ -16,7 +16,7 @@
     FlutterMethodChannel* nativeChannel = [FlutterMethodChannel methodChannelWithName:@"com.bugsnag.mazeRunner/platform"
               binaryMessenger:controller.engine.binaryMessenger
     ];
-     
+
     [nativeChannel setMethodCallHandler:^(FlutterMethodCall *call, FlutterResult result) {
         [self onMethod :call :result];
     }];
@@ -52,20 +52,9 @@
 
 
 -(NSString *)getCommand {
-//    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration ephemeralSessionConfiguration]];
-//    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://bs-local.com:9339/command"]];
     NSURL *url = [NSURL URLWithString:@"http://bs-local.com:9339/command"];
     NSData *data = [NSData dataWithContentsOfURL:url];
     NSString *ret = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-    
-//    [[session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-//        if (![response isKindOfClass:[NSHTTPURLResponse class]] || [(NSHTTPURLResponse *)response statusCode] != 200) {
-//            NSLog(@"%s request failed with %@", __PRETTY_FUNCTION__, response ?: error);
-//            return @"{}";
-//        }
-//        NSString *command = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-//        return command;
-//    }] resume];
 
     return ret;
 }
--- features/fixtures/app/lib/scenarios/native_crash_scenario.dart
+++ features/fixtures/app/lib/scenarios/native_crash_scenario.dart
@@ -5,7 +5,6 @@ import 'scenario.dart';
 class NativeCrashScenario extends Scenario {
   @override
   Future<void> run() async {
-    // TODO: I think I would factor startBugsnag() out of the scenarios and into main to avoid repetition
     await startBugsnag();
     await MazeRunnerChannels.runScenario("NativeCrashScenario");
   }
