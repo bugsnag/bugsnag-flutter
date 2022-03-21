3eebad37291715309c9502427e4975fba5600eae
Jason <jason.morris@bugsnag.com>
2022-03-21 17:53:06 +0000

    fix(session): fixed Session / OnSession callbacks to line-up with the Notifier spec

--- bugsnag_flutter/lib/src/model/session.dart
+++ bugsnag_flutter/lib/src/model/session.dart
@@ -1,30 +1,18 @@
-import 'app.dart';
-import 'device.dart';
-import 'notifier.dart';
 import 'user.dart';
 
 class Session {
   String id;
   DateTime startedAt;
   User user;
-  App app;
-  Device device;
-  Notifier notifier;
 
   Session.fromJson(Map<String, dynamic> json)
       : id = json['id'] as String,
         startedAt = DateTime.parse(json['startedAt']),
-        user = User.fromJson(json['user'] as Map<String, dynamic>),
-        app = App.fromJson(json['app'] as Map<String, dynamic>),
-        device = Device.fromJson(json['device'] as Map<String, dynamic>),
-        notifier = Notifier.fromJson(json['notifier'] as Map<String, dynamic>);
+        user = User.fromJson(json['user'] as Map<String, dynamic>);
 
   dynamic toJson() => {
         'id': id,
         'startedAt': startedAt.toIso8601String(),
         'user': user,
-        'app': app,
-        'device': device,
-        'notifier': notifier,
       };
 }
