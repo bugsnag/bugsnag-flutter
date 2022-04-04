f9d884ad62a82fcf2900a26de3d792cbbccd0c38
Jason <jason.morris@bugsnag.com>
2022-04-04 11:53:27 +0100

    fix(stacktrace): obfuscated stack traces should use absolute addresses (base address + frame offset) in `frameAddress`

--- bugsnag_flutter/lib/src/bugsnag_stacktrace.dart
+++ bugsnag_flutter/lib/src/bugsnag_stacktrace.dart
@@ -6,16 +6,16 @@ import 'package:flutter/foundation.dart';
 // the primary difference is that we're only interested in the virtual address
 
 final _traceLineRE = RegExp(
-    r'\s*#(\d+) abs [\da-f]+(?: virt (?<virtual>[\da-f]+))? (?<rest>.*)$');
+    r'\s*#(\d+) abs (?<absolute>[\da-f]+)(?: virt (?<virtual>[\da-f]+))? (?<rest>.*)$');
 
 final _buildIdRegExp = RegExp(r"build_id: \'([a-f0-9]+)\'");
 final _baseAddressRegExp = RegExp(r"isolate_dso_base: ([a-f0-9]+),");
 
 class _Frame {
-  final int offset;
+  final int absoluteAddress;
   final String? method;
 
-  const _Frame(this.offset, this.method);
+  const _Frame(this.absoluteAddress, this.method);
 
   static final _symbolOffsetRE =
       RegExp(r'(?<symbol>\w+)\+(?<offset>(?:0x)?[\da-f]+)');
@@ -23,15 +23,11 @@ class _Frame {
   static _Frame? parse(String line) {
     final match = _traceLineRE.firstMatch(line);
     if (match == null) return null;
-    // If all other cases failed, check for a virtual address. Until this package
-    // depends on a version of Dart which only prints virtual addresses when the
-    // virtual addresses in the snapshot are the same as in separately saved
-    // debugging information, the other methods should be tried first.
-    final virtualString = match.namedGroup('virtual');
+    final addressString = match.namedGroup('absolute');
     final rest = match.namedGroup('rest');
 
     final address =
-        virtualString != null ? int.tryParse(virtualString, radix: 16) : null;
+        addressString != null ? int.tryParse(addressString, radix: 16) : null;
 
     if (address != null) {
       return _Frame(address, _extractMethodName(rest));
@@ -57,16 +53,12 @@ String? _parseBuildId(String line) {
 }
 
 // Try to parse the Isolate base address from the line (isolate_dso_base)
-String? _parseBaseAddress(String line) {
+int? _parseBaseAddress(String line) {
   final match = _baseAddressRegExp.firstMatch(line);
   final matchedAddressString = match?.group(1);
 
   if (matchedAddressString != null) {
-    // we parse and re-format the address to remove any padding, etc.
-    final matchedAddress = int.tryParse(matchedAddressString, radix: 16);
-    if (matchedAddress != null) {
-      return '0x${matchedAddress.toRadixString(16)}';
-    }
+    return int.tryParse(matchedAddressString, radix: 16);
   }
 
   return null;
@@ -88,19 +80,19 @@ Stacktrace? parseNativeStackTrace(String stackTrace) {
   final stackTraceLines = stackTrace.split('\n');
 
   String? buildId;
-  String? baseOffsetString;
+  int? baseOffset;
 
   List<Stackframe> stacktrace = [];
 
   for (final line in stackTraceLines) {
     buildId ??= _parseBuildId(line);
-    baseOffsetString ??= _parseBaseAddress(line);
+    baseOffset ??= _parseBaseAddress(line);
     final frame = _Frame.parse(line);
 
-    if (frame != null) {
+    if (frame != null && baseOffset != null) {
       stacktrace.add(Stackframe(
-        frameAddress: '0x' + frame.offset.toRadixString(16),
-        loadAddress: baseOffsetString,
+        frameAddress: '0x' + (frame.absoluteAddress).toRadixString(16),
+        loadAddress: '0x' + baseOffset.toRadixString(16),
         codeIdentifier: buildId,
         method: frame.method,
       ));
