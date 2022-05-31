d538b52083d970753eaa9f849584faf6688501d3
Josh Edney <josh.edney@smartbear.com>
2022-05-31 15:54:11 +0100

    chore: update FLUTTER exe variable

--- features/scripts/build_ios_app.sh
+++ features/scripts/build_ios_app.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit
 
-if [ -z "$FLUTTER_DIR" ]; then
-  FLUTTER_DIR="flutter"
+if [ -z "$FLUTTER_BIN" ]; then
+  FLUTTER_BIN="flutter"
 fi
 
+echo "Flutter Bin: $FLUTTER_BIN"
+
 cd features/fixtures/app
-$FLUTTER_DIR build ipa --export-options-plist=ios/exportOptions.plist
+$FLUTTER_BIN build ipa --export-options-plist=ios/exportOptions.plist
