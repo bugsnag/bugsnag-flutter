ecad7d81f71b1535d2abf356fbf3845bd68572e4
Nick Dowell <nick@bugsnag.com>
2022-07-25 14:31:30 +0100

    Set `projectPackages` in examples/native

--- examples/native/android/app/src/main/java/com/example/bugsnag/flutter/android/ExampleApp.kt
+++ examples/native/android/app/src/main/java/com/example/bugsnag/flutter/android/ExampleApp.kt
@@ -2,14 +2,20 @@ package com.example.bugsnag.flutter.android
 
 import android.app.Application
 import com.bugsnag.android.Bugsnag
+import com.bugsnag.android.Configuration
 import com.bugsnag.flutter.BugsnagFlutterConfiguration
 
 class ExampleApp : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        val config = Configuration.load(this)
+
+        // Add the names of Dart packages that should be displayed as "in-project" on your dashboard
+        config.projectPackages = setOf(packageName, "example_flutter")
+
         // Start Bugsnag Android SDK
-        Bugsnag.start(this)
+        Bugsnag.start(this, config)
 
         // Uncomment to disable automatic detection of Dart errors:
         // BugsnagFlutterConfiguration.enabledErrorTypes.dartErrors = false
