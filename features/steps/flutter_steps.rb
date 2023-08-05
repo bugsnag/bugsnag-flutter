a6849ebddabd9573825a431d2cd7b1715f7a570c
Robert <robert.smartbear@gmail.com>
2023-08-05 10:05:18 +0200

    Added waiting for runCommand button

--- features/steps/flutter_steps.rb
+++ features/steps/flutter_steps.rb
@@ -22,6 +22,7 @@ def execute_command(action, scenario_name)
   command = { action: action, scenario_name: scenario_name, extra_config: extra_config }
   Maze::Server.commands.add command
 
+  Maze.driver.wait_for_element('runCommand', 30, true)
   touch_action = Appium::TouchAction.new
   touch_action.tap({:x => 200, :y => 200})
   touch_action.perform
