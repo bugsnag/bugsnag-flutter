d2a4790746cb003ac4ab13e4f32c26a4c24f04f8
Vinicius Mangueira Correia <vinicius.correia@willbank.com.br>
2024-08-12 15:51:36 -0300

    chore: adding missing header files

---
+++ packages/bugsnag_flutter/ios/Classes/BugsnagNotifier.h
@@ -0,0 +1,34 @@
+//
+//  BugsnagNotifier.h
+//  Bugsnag
+//
+//  Created by Jamie Lynch on 29/04/2020.
+//  Copyright © 2020 Bugsnag. All rights reserved.
+//
+
+#import <Bugsnag/BugsnagDefines.h>
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+BUGSNAG_EXTERN
+@interface BugsnagNotifier : NSObject
+
+/// Initializes the object with details of the Cocoa notifier.
+- (instancetype)init NS_DESIGNATED_INITIALIZER;
+
+- (instancetype)initWithName:(NSString *)name
+                     version:(NSString *)version
+                         url:(NSString *)url
+                dependencies:(NSArray<BugsnagNotifier *> *)dependencies NS_DESIGNATED_INITIALIZER;
+
+@property (copy, nonatomic) NSString *name;
+@property (copy, nonatomic) NSString *version;
+@property (copy, nonatomic) NSString *url;
+@property (copy, nonatomic) NSArray<BugsnagNotifier *> *dependencies;
+
+- (NSDictionary *)toDict;
+
+@end
+
+NS_ASSUME_NONNULL_END
