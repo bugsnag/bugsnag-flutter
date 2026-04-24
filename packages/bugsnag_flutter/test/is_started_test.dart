b7c7e2c6934208c409de5a1f05aeb16f7af90f0d
sb-adityap <aditya.patil@smartbear.com>
2026-04-24 18:49:08 +0530

    test: add isStarted state transition tests for start and attach flows

--- packages/bugsnag_flutter/test/is_started_test.dart
+++ packages/bugsnag_flutter/test/is_started_test.dart
@@ -6,18 +6,18 @@ import 'test_channel.dart';
 void main() {
   final channel = MockChannelClientController();
 
-  group('ChannelClient.isStarted', () {
+  group('isStarted', () {
     setUp(() {
       channel.reset();
     });
 
     test('ChannelClient has isStarted property that returns bool', () {
       final client = ChannelClient(true);
-      
+
       // isStarted should be accessible and return a bool
       final isStarted = client.isStarted;
       expect(isStarted, isA<bool>());
-      
+
       // Initially should be false
       expect(isStarted, isFalse);
     });
@@ -25,12 +25,33 @@ void main() {
     test('ChannelClient.isStarted is false on new instances', () {
       final client1 = ChannelClient(true);
       final client2 = ChannelClient(false);
-      
+
       expect(client1.isStarted, isFalse);
       expect(client2.isStarted, isFalse);
     });
+
+    test('bugsnag.start transitions isStarted to true', () async {
+      channel.results['start'] = true;
+
+      await bugsnag.start(autoTrackSessions: false);
+
+      expect(bugsnag.isStarted, isTrue);
+      expect((bugsnag.client as ChannelClient).isStarted, isTrue);
+      expect(channel['start'], hasLength(1));
+    });
+
+    test('bugsnag.attach transitions isStarted to true', () async {
+      channel.results['attach'] = {
+        'config': {
+          'enabledErrorTypes': {'dartErrors': true}
+        }
+      };
+
+      await bugsnag.attach();
+
+      expect(bugsnag.isStarted, isTrue);
+      expect((bugsnag.client as ChannelClient).isStarted, isTrue);
+      expect(channel['attach'], hasLength(1));
+    });
   });
 }
-
-
-
